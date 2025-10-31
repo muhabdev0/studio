d1c844e4916ea897831ab4c8eed2372da0d66cc2
Muhab Designs <muhabulfi@gmail.com>
2025-10-31 05:32:40 +0000

    make another logo i didnt like it i want it to represent the letter s

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -42,8 +42,8 @@ export function AppSidebar() {
     <Sidebar>
       <SidebarHeader>
         <div className="flex items-center gap-2">
-          <div className="flex h-8 w-8 items-center justify-center rounded-lg bg-primary text-primary-foreground">
-            <svg
+          <div className="flex h-8 w-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="none"
@@ -53,11 +53,7 @@ export function AppSidebar() {
               strokeLinejoin="round"
               className="h-5 w-5"
             >
-              <path d="M13.4 2.6L10.6 2.6C8.4 2.6 7.5 4.3 8.1 6.3L10.3 12.8C10.5 13.5 11.2 14 11.9 14H12.1C12.8 14 13.5 13.5 13.7 12.8L15.9 6.3C16.5 4.3 15.6 2.6 13.4 2.6" />
-              <path d="M10.3 12.8L8.1 6.3C7.5 4.3 6.6 2.6 4.4 2.6L1.6 2.6" />
-              <path d="M13.7 12.8L15.9 6.3C16.5 4.3 17.4 2.6 19.6 2.6L22.4 2.6" />
-              <path d="M12 14L10.7 18.2C10.4 19.4 9.4 20.3 8.2 20.3L6.3 20.3C4.8 20.3 4 21.6 4.7 22.9L5.3 23.9" />
-              <path d="M12 14L13.3 18.2C13.6 19.4 14.6 20.3 15.8 20.3L17.7 20.3C19.2 20.3 20 21.6 19.3 22.9L18.7 23.9" />
+              <path d="M6 8.25C8.5 6.25 12.5 5.25 15.5 6.75C18.5 8.25 20.5 12 18 15.5C15.5 19 11.5 20 8.5 18.5C5.5 17 3.5 13.25 6 9.75" />
             </svg>
           </div>
           <span className="font-semibold text-lg">SwiftRoute</span>
